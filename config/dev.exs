6bcd686528715063ceee51a1d6766c8691beb948
Maksym Kolodnytskyi <mcsym28+macos@gmail.com>
2024-06-04 16:20:30 +0300

    Initial generator implementation

--- config/dev.exs
+++ config/dev.exs
@@ -4,18 +4,71 @@ import Config
 config :logger, :console, format: "[$level] $message\n"
 
 config :oapi_generator,
-  default: [
-    renderer: OpenAPIGenerator.Renderer,
+  acquiring: [
+    processor: OpenAPIClient.Generator.Processor,
+    renderer: OpenAPIClient.Generator.Renderer,
+    naming: [
+      rename: [
+        {~r/^(.+)Error$/, "Errors.\\1"},
+        {~r/^Qr(.+)$/, "QR.\\1"},
+        {~r/^QR\.ListItem$/, "QR.ListResponse.Item"},
+        {~r/^QR\.ResetRequest$/, "QR.ResetAmountRequest"},
+        {~r/^PubkeyResponse$/, "Merchants.PubkeyResponse"},
+        {~r/^Invoice([^s].+)$/, "Invoices.\\1"},
+        {~r/^FiscalizationItem$/, "Invoices.FiscalizationItem"},
+        {~r/^Invoices\.SyncPaymentRequest(.*)$/, "Invoices.CreateSyncPaymentRequest\\1"},
+        {~r/^Invoices\.CreateSyncPaymentRequest([^\.].+)$/,
+         "Invoices.CreateSyncPaymentRequest.\\1"},
+        {~r/^Invoices\.StatusResponse([^\.].+)$/, "Invoices.StatusResponse.\\1"},
+        {~r/^Invoices\.CreateRequest([^\.].+)$/, "Invoices.CreateRequest.\\1"},
+        {~r/^CancelListItem$/, "Invoices.CancelListItem"},
+        {~r/^PaymentDirectRequest(.*)$/, "Invoices.CreateDirectPaymentRequest\\1"},
+        {~r/^Invoices\.CreateDirectPaymentRequest([^\.].+)$/,
+         "Invoices.CreateDirectPaymentRequest.\\1"},
+        {~r/^FiscalCheckListResponse(.*)$/, "Invoices.FiscalChecksResponse\\1"},
+        {~r/^(Invoices\.FiscalChecksResponse)Checks$/, "\\1.Check"},
+        {~r/^MerchantStatementResponse(.*)$/, "Merchants.StatementsResponse\\1"},
+        {~r/^MerchantStatement(Item.*)$/, "Merchants.StatementsResponse.List\\1"},
+        {~r/^Merchants\.StatementsResponse\.ListItemCancelList$/,
+         "Merchants.StatementsResponse.ListItem.CancelListItem"},
+        {~r/^MerchantWalletResponse(.*)$/, "Wallets.GetResponse\\1"},
+        {~r/^WalletItem$/, "Wallets.GetResponse.ListItem"},
+        {~r/^MerchantWalletPayment(.*)$/, "Wallets.CreatePayment\\1"},
+        {~r/^MerchantDetailsResponse$/, "Merchants.DetailsResponse"},
+        {~r/^MerchantPaymInfoItem$/, "Merchants.PaymentInfo"},
+        {~r/^Merchants\.PaymentInfo([^\.].+)$/, "Merchants.PaymentInfo.\\1"},
+        {~r/^Merchants\.PaymentInfo\.BasketOrderDiscounts$/,
+         "Merchants.PaymentInfo.BasketOrder.Discount"},
+        {~r/^SubmerchantListResponse$/, "Merchants.Submerchants.ListResponse"},
+        {~r/^Merchants\.Submerchants\.ListResponseList$/,
+         "Merchants.Submerchants.ListResponse.Item"},
+        {~r/^Invoices\.SyncPaymentRequest\.MerchantPaymInfo$/,
+         "Invoices.SyncPaymentRequest.MerchantPaymentInfo"},
+        {~r/^EmployeeListResponse$/, "Merchants.Employees.ListResponse"},
+        {~r/^Merchants\.Employees\.ListResponseList$/, "Merchants.Employees.ListResponse.Item"}
+      ]
+    ],
     output: [
-      base_module: Default,
-      location: "lib"
+      base_module: MonobankAPI.Acquiring,
+      location: "lib/acquiring"
     ]
   ]
 
-config :open_api_generator_ex,
-  default: [
+config :open_api_client_ex,
+  "$base": [
+    client_pipeline: OpenAPIClient.BasicHTTPoisonPipeline,
+    httpoison: OpenAPIClient.HTTPoisonMock,
+    decoders: [
+      {"application/json", {Jason, :decode, []}}
+    ],
+    encoders: [
+      {"application/json", {Jason, :encode, []}}
+    ]
+  ],
+  acquiring: [
+    base_url: "https://api.monobank.ua",
     operations: [
-      {:all,
+      {:*,
        [
          params: [
            {{"X-Token", :header},
@@ -29,5 +82,6 @@ config :open_api_generator_ex,
             ]}
          ]
        ]}
-    ]
+    ],
+    test_location: "test/acquiring"
   ]
--- mix.exs
+++ mix.exs
@@ -7,9 +7,18 @@ defmodule MonobankAPI.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.html": :test
+      ]
     ]
   end
+
+  defp elixirc_paths(:dev), do: ["lib_dev" | elixirc_paths(:prod)]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -21,14 +30,15 @@ defmodule MonobankAPI.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:open_api_client_ex, git: "../../../open-api-client-ex"},
+      {:jason, "~> 1.4", optional: true, only: [:dev, :test]},
+      {:httpoison, "~> 2.2", optional: true, only: [:dev, :test]},
       {:oapi_generator,
-       git: "../../../open-api-generator",
-       branch: "behaviour_impl",
-       only: ~w(test dev)a,
-       runtime: false,
-       override: true},
-      {:open_api_client_ex,
-       git: "../../../open-api-client-ex", only: ~w(test dev)a, runtime: false}
+       git: "../../../open-api-generator", branch: "behaviour_impl", only: [:dev, :test]},
+      {:mox, "~> 1.1", only: [:dev, :test]},
+      {:floki, "~> 0.36", only: [:dev, :test]},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 end
